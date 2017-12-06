ee3fdd50c82d223118303f8921a20e9d4e7d5b33
robertzml <robertzml@126.com>
2017-12-06 22:13:52 +0800

    edit workload biz.
    Signed-off-by: robertzml <robertzml@126.com>

--- Hades.HR.Core/BLL/Attendance/LaborDailyWorkload.cs
+++ Hades.HR.Core/BLL/Attendance/LaborDailyWorkload.cs
@@ -16,9 +16,29 @@ namespace Hades.HR.BLL
     /// </summary>
 	public class LaborDailyWorkload : BaseBLL<LaborDailyWorkloadInfo>
     {
+        #region Constructor
         public LaborDailyWorkload() : base()
         {
             base.Init(this.GetType().FullName, System.Reflection.Assembly.GetExecutingAssembly().GetName().Name);
         }
+        #endregion //Constructor
+
+        #region Method
+        /// <summary>
+        /// 检查员工是否已在其它班组
+        /// </summary>
+        /// <param name="date">日期</param>
+        /// <param name="workTeamId">当前班组ID</param>
+        /// <param name="laborId">员工ID</param>
+        /// <returns></returns>
+        public bool CheckLaborDailyExist(DateTime date, string workTeamId, string laborId)
+        {
+            var labors = this.Find(string.Format("AttendanceDate = '{0}' AND ActualWorkTeamId != '{1}' AND StaffId = '{2}'", date, workTeamId, laborId));
+            if (labors.Count > 0)
+                return true;
+            else
+                return false;
+        }
+        #endregion //Method
     }
 }
--- Hades.HR.Core/BLL/Attendance/WorkTeamDailyWorkload.cs
+++ Hades.HR.Core/BLL/Attendance/WorkTeamDailyWorkload.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Data;
 using System.Data.Common;
 using System.Collections.Generic;
+using System.Linq;
 
 using Hades.HR.Entity;
 using Hades.HR.IDAL;
@@ -43,13 +44,21 @@ namespace Hades.HR.BLL
 
             try
             {
+                //check labors not in other team
+                LaborDailyWorkload laborDailyWorkload = new LaborDailyWorkload();
+                foreach(var item in labors)
+                {
+                    var exist = laborDailyWorkload.CheckLaborDailyExist(workTeam.AttendanceDate, workTeam.Id, item.StaffId);
+                    if (exist)
+                        return false;
+                }
+
                 workTeam.PersonCount = labors.Count;
                 dal.Insert(workTeam, trans);
 
                 foreach (var item in labors)
                 {
-                    LaborDailyWorkload laborBll = new LaborDailyWorkload();
-                    laborBll.Insert(item, trans);
+                    laborDailyWorkload.Insert(item, trans);
                 }
 
                 if (isLocalTrans)
@@ -57,6 +66,131 @@ namespace Hades.HR.BLL
                     trans.Commit();
                 }
 
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (isLocalTrans)
+                {
+                    trans.Rollback();
+                }
+                LogTextHelper.Error("初始化班组日考勤", e);
+                return false;
+            }
+            finally
+            {
+                if (isLocalTrans)
+                {
+                    trans = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 更新班组日考勤员工
+        /// </summary>
+        /// <param name="workTeam">班组日考勤</param>
+        /// <param name="labors">员工日考勤</param>
+        /// <param name="trans"></param>
+        /// <returns></returns>
+        public bool UpdateDailyLabor(WorkTeamDailyWorkloadInfo workTeam, List<LaborDailyWorkloadInfo> labors, DbTransaction trans = null)
+        {
+            var dal = this.baseDal as IWorkTeamDailyWorkload;
+
+            bool isLocalTrans = trans == null;
+            if (isLocalTrans)
+            {
+                trans = dal.CreateTransaction();
+            }
+
+            try
+            {
+                //check labors not in other team
+                LaborDailyWorkload laborDailyWorkload = new LaborDailyWorkload();
+                foreach (var item in labors)
+                {
+                    var exist = laborDailyWorkload.CheckLaborDailyExist(workTeam.AttendanceDate, workTeam.Id, item.StaffId);
+                    if (exist)
+                        return false;
+                }
+
+                workTeam.PersonCount = labors.Count;
+                dal.Update(workTeam, workTeam.Id, trans);
+
+                laborDailyWorkload.DeleteByCondition(string.Format("WorkTeamWorkloadId = '{0}'", workTeam.Id), trans);
+
+                foreach (var item in labors)
+                {
+                    laborDailyWorkload.Insert(item, trans);
+                }
+
+                if (isLocalTrans)
+                {
+                    trans.Commit();
+                }
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                if (isLocalTrans)
+                {
+                    trans.Rollback();
+                }
+                LogTextHelper.Error("初始化班组日考勤", e);
+                return false;
+            }
+            finally
+            {
+                if (isLocalTrans)
+                {
+                    trans = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 保存产量工时信息
+        /// </summary>
+        /// <param name="workTeamWorkloadId">班组日工作量ID</param>
+        /// <param name="productionHours">产量总工时</param>
+        /// <param name="productWorkloads">员工产量工作量信息</param>
+        /// <param name="trans"></param>
+        /// <returns></returns>
+        public bool SaveProduction(string workTeamWorkloadId, decimal productionHours, List<LaborProductionWorkloadInfo> productWorkloads, DbTransaction trans = null)
+        {
+            var dal = this.baseDal as IWorkTeamDailyWorkload;
+
+            bool isLocalTrans = trans == null;
+            if (isLocalTrans)
+            {
+                trans = dal.CreateTransaction();
+            }
+
+            try
+            {
+                var workTeamWorkload = dal.FindByID(workTeamWorkloadId, trans);
+                workTeamWorkload.ProductionHours = productionHours;
+
+                dal.Update(workTeamWorkload, workTeamWorkload.Id, trans);
+
+                LaborProductionWorkload productBll = new LaborProductionWorkload();
+                foreach (var item in productWorkloads)
+                {
+                    // 增加产量工时分配
+                    productBll.Insert(item);
+                }
+
+                LaborDailyWorkload laborWorkloadBll = new LaborDailyWorkload();
+                List<LaborDailyWorkloadInfo> laborWorkloads = laborWorkloadBll.Find(string.Format("WorkTeamWorkloadId='{0}'", workTeamWorkloadId));
+
+                foreach(var item in laborWorkloads)
+                {
+                    var hours = productWorkloads.Where(r => r.StaffId == item.StaffId).Sum(r => r.ProductionHours);
+                    item.ProductionHours = hours;
+
+                    laborWorkloadBll.Update(item, item.Id);
+                }
                 return true;
             }
             catch (Exception e)
